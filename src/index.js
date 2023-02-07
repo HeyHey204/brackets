4e5c15fcdd5b5e0f42ed8da1e53d988c84853301
Alexey Gerasimov <alexger204@gmail.com>
2023-02-07 23:16:00 +0500

    init: finished task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	let stack = [];
+	let pairs = {};
+	bracketsConfig.forEach(bracket => {
+		pairs[bracket[0]] = bracket[1];
+	});
+
+	for (let i = 0; i < str.length; i++) {
+		let char = str[i];
+		if (pairs[char]) {
+			stack.push(char);
+		} else {
+			for (let key in pairs) {
+				if (char === pairs[key]) {
+					let last = stack.pop();
+					if (last === undefined || last !== key) {
+						return false;
+					}
+				}
+			}
+		}
+	}
+	return stack.length === 0;
 }
